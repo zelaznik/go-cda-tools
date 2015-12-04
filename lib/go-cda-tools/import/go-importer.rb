e0bb426f0ad1a6dc3cd63c782e38b726513a0e8a
Andrew Hubley <ahubley@mitre.org>
2015-12-04 15:00:26 -0500

    refactor to extract go cda package to own repository

--- lib/go-cda-tools/import/go-importer.rb
+++ lib/go-cda-tools/import/go-importer.rb
@@ -8,10 +8,10 @@ module GoCDATools
       include Singleton
       extend FFI::Library
         ffi_lib File.expand_path("../../../ext/libgocda.so", File.dirname(__FILE__))
-        attach_function :read_patient, [:string], :string
+        attach_function :import_cat1, [:string], :string
 
         def parse_with_ffi(path)
-          patient_json_string = read_patient(path)
+          patient_json_string = import_cat1(path)
           patient = Record.new(JSON.parse(patient_json_string))
           patient
         end
