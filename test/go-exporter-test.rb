3e60730b5658018490c3605cf15ce19217a3f931
Andrew Schreiber <aschreiber@VA00029-PC.local>
2016-01-05 10:00:24 -0500

    Added printing of error messages to the test failures.

--- test/go-exporter-test.rb
+++ test/go-exporter-test.rb
@@ -4,6 +4,6 @@ class GoExporterTest < Minitest::Test
   def test_valid_cda
     cda = GoCDATools::Export::GoExporter.instance.export_with_ffi(File.read('test/fixtures/records/barry_berry.json'))
     errors = HealthDataStandards::Validate::CDA.instance().validate(cda)
-    assert errors.empty?, "CDA Validation had #{errors.length} error(s)."
+    assert errors.empty?, "CDA Validation had #{errors.length} error(s). \n \t#{errors.map{|e| e.message}.join("\n\t")}"
   end
 end
