ebcb22a2ac102c49e1245d582ad09d8b63ada75f
Michael O'Keefe <mokeefe@mitre.org>
2016-03-08 11:17:23 -0500

    Added start and end dates to FFI method signatures, and test files

--- ext/bindings.go
+++ ext/bindings.go
@@ -16,8 +16,10 @@ func import_cat1(rawPath *C.char) *C.char {
 }
 
 //export generateCat1
-func generateCat1(patient *C.char, measures *C.char) *C.char {
+func generateCat1(patient *C.char, measures *C.char, startDate C.longlong, endDate C.longlong) *C.char {
 	patientbytes := []byte(C.GoString(patient))
 	measuresbytes := []byte(C.GoString(measures))
-	return C.CString(exporter.GenerateCat1(patientbytes, measuresbytes))
+	startDateInt := int64(startDate)
+	endDateInt := int64(endDate)
+	return C.CString(exporter.GenerateCat1(patientbytes, measuresbytes, startDateInt, endDateInt))
 }
