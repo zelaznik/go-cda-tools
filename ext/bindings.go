6fbfcf6702c3a26cc4fa67c6156eed05058588f0
Rob Dingwell <bobd@mitre.org>
2016-04-26 10:17:44 -0400

    updating the signatire call to cdatools exprot functions which now take valuesets , start and end times

--- ext/bindings.go
+++ ext/bindings.go
@@ -15,9 +15,12 @@ func import_cat1(rawPath *C.char) *C.char {
 	return C.CString(importer.Read_patient(path))
 }
 
+//GenerateCat1(patient []byte, measures []byte, valueSets []byte, startDate int64, endDate int64) string {
+
 //export generateCat1
-func generateCat1(patient *C.char, measures *C.char) *C.char {
+func generateCat1(patient *C.char, measures *C.char,valueSets *C.char, startDate int64, endDate int64) *C.char {
 	patientbytes := []byte(C.GoString(patient))
 	measuresbytes := []byte(C.GoString(measures))
-	return C.CString(exporter.GenerateCat1(patientbytes, measuresbytes))
+	valueSetBytes := []byte(C.GoString(valueSets))
+	return C.CString(exporter.GenerateCat1(patientbytes, measuresbytes, valueSetBytes, startDate,endDate))
 }
--- lib/go-cda-tools/export/go-exporter.rb
+++ lib/go-cda-tools/export/go-exporter.rb
@@ -7,10 +7,10 @@ module GoCDATools
       include Singleton
       extend FFI::Library
         ffi_lib File.expand_path("../../../ext/libgocda.so", File.dirname(__FILE__))
-        attach_function :generateCat1, [:string, :string], :string
+        attach_function :generateCat1, [:string, :string, :string, :int64,:int64], :string
 
-        def export_with_ffi(obj, mes)
-          generateCat1(obj, mes)
+        def export_with_ffi(obj, mes,valuesets,startDate,endDate)
+          generateCat1(obj, mes,valuesets,startDate,endDate)
         end
 
     end
