f2bfbc3f48dc98dd83d423c3e77ec87c103aee69
Tom Strassner <tstrassner@mitre.org>
2017-03-13 13:55:16 -0400

    Add bindings for go export

--- lib/go-cda-tools/export/go-exporter.rb
+++ lib/go-cda-tools/export/go-exporter.rb
@@ -5,14 +5,13 @@ module GoCDATools
   module Export
     class GoExporter
       include Singleton
-      # extend FFI::Library
-        # ffi_lib File.expand_path("../../../ext/libgocda.so", File.dirname(__FILE__))
-        # attach_function :generateCat1, [:string, :string], :string
-        #
-        # def export_with_ffi(obj, mes)
-        #   generateCat1(obj, mes)
-        # end
+      extend FFI::Library
+        ffi_lib File.expand_path("../../../ext/libgocda-mac.so", File.dirname(__FILE__))
+        attach_function :generateCat1, [:string, :string, :string, :int, :int, :string], :string
 
+        def export_with_ffi(patient, measures, value_sets, start_date, end_date, qrda_version)
+          generateCat1(patient, measures, value_sets, start_date.to_i, end_date.to_i, qrda_version)
+        end
     end
   end
 end
